725bfbda27073f5c63a7f189887ef894c0855335
Yuri.Sh <yuri@bynet.co.il>
2013-02-15 19:17:55 +0200

    JB KERNEL NEW SOURCE! drivers/samsung + drivers/video/

--- drivers/video/samsung/mdnie_table_p4note.h
+++ drivers/video/samsung/mdnie_table_p4note.h
@@ -416,8 +416,8 @@ static const unsigned short tune_movie_ui[] = {
 	0x00e8,  0x00ff,	/*SCR BgYg*/
 	0x00e9,  0xff00,	/*SCR BbYb*/
 	0x00ea,  0x00ff,	/*SCR KrWr*/
-	0x00eb,  0x00fa,	/*SCR KgWg*/
-	0x00ec,  0x00f3,	/*SCR KbWb*/
+	0x00eb,  0x00f6,	/*SCR KgWg*/
+	0x00ec,  0x00e5,	/*SCR KbWb*/
 	0x0000,  0x0001,	/*BANK 1*/
 	0x001f,  0x0080,	/*CC chsel strength*/
 	0x0020,  0x0000,	/*CC lut r	 0*/
@@ -457,8 +457,8 @@ static const unsigned short tune_movie_video[] = {
 	0x00e8,  0x00ff,	/*SCR BgYg*/
 	0x00e9,  0xff00,	/*SCR BbYb*/
 	0x00ea,  0x00ff,	/*SCR KrWr*/
-	0x00eb,  0x00fa,	/*SCR KgWg*/
-	0x00ec,  0x00f3,	/*SCR KbWb*/
+	0x00eb,  0x00f6,	/*SCR KgWg*/
+	0x00ec,  0x00e5,	/*SCR KbWb*/
 	0x0000,  0x0001,	/*BANK 1*/
 	0x001f,  0x0080,	/*CC chsel strength*/
 	0x0020,  0x0000,	/*CC lut r	 0*/
@@ -488,8 +488,8 @@ static const unsigned short tune_movie_gallery[] = {
 	0x00e8,  0x00ff,	/*SCR BgYg*/
 	0x00e9,  0xff00,	/*SCR BbYb*/
 	0x00ea,  0x00ff,	/*SCR KrWr*/
-	0x00eb,  0x00fa,	/*SCR KgWg*/
-	0x00ec,  0x00f3,	/*SCR KbWb*/
+	0x00eb,  0x00f6,	/*SCR KgWg*/
+	0x00ec,  0x00e5,	/*SCR KbWb*/
 	0x0000,  0x0001,	/*BANK 1*/
 	0x001f,  0x0080,	/*CC chsel strength*/
 	0x0020,  0x0000,	/*CC lut r	 0*/
@@ -892,8 +892,8 @@ static const unsigned short tune_movie_ui_cabc[] = {
 	0x00e8,  0x00ff,	/*SCR BgYg*/
 	0x00e9,  0xff00,	/*SCR BbYb*/
 	0x00ea,  0x00ff,	/*SCR KrWr*/
-	0x00eb,  0x00fa,	/*SCR KgWg*/
-	0x00ec,  0x00f3,	/*SCR KbWb*/
+	0x00eb,  0x00f6,	/*SCR KgWg*/
+	0x00ec,  0x00e5,	/*SCR KbWb*/
 	0x0000,  0x0001,	/*BANK 1*/
 	0x001f,  0x0080,	/*CC chsel strength*/
 	0x0020,  0x0000,	/*CC lut r	 0*/
@@ -938,8 +938,8 @@ static const unsigned short tune_movie_video_cabc[] = {
 	0x00e8,  0x00ff,	/*SCR BgYg*/
 	0x00e9,  0xff00,	/*SCR BbYb*/
 	0x00ea,  0x00ff,	/*SCR KrWr*/
-	0x00eb,  0x00fa,	/*SCR KgWg*/
-	0x00ec,  0x00f3,	/*SCR KbWb*/
+	0x00eb,  0x00f6,	/*SCR KgWg*/
+	0x00ec,  0x00e5,	/*SCR KbWb*/
 	0x0000,  0x0001,	/*BANK 1*/
 	0x001f,  0x0080,	/*CC chsel strength*/
 	0x0020,  0x0000,	/*CC lut r	 0*/
@@ -974,8 +974,8 @@ static const unsigned short tune_movie_gallery_cabc[] = {
 	0x00e8,  0x00ff,	/*SCR BgYg*/
 	0x00e9,  0xff00,	/*SCR BbYb*/
 	0x00ea,  0x00ff,	/*SCR KrWr*/
-	0x00eb,  0x00fa,	/*SCR KgWg*/
-	0x00ec,  0x00f3,	/*SCR KbWb*/
+	0x00eb,  0x00f6,	/*SCR KgWg*/
+	0x00ec,  0x00e5,	/*SCR KbWb*/
 	0x0000,  0x0001,	/*BANK 1*/
 	0x001f,  0x0080,	/*CC chsel strength*/
 	0x0020,  0x0000,	/*CC lut r	 0*/
--- drivers/video/samsung/s6d6aa1.c
+++ drivers/video/samsung/s6d6aa1.c
@@ -258,10 +258,11 @@ static irqreturn_t oled_detection_int(int irq, void *_lcd)
 #endif
 
 
-static int s6d6aa1_write(struct lcd_info *lcd, const unsigned char *seq, int len)
+static int _s6d6aa1_write(struct lcd_info *lcd, const unsigned char *seq, int len)
 {
 	int size;
 	const unsigned char *wbuf;
+	int ret = 0;
 
 	if (!lcd->connected)
 		return 0;
@@ -272,15 +273,34 @@ static int s6d6aa1_write(struct lcd_info *lcd, const unsigned char *seq, int len
 	wbuf = seq;
 
 	if (size == 1)
-		lcd->dsim->ops->cmd_write(lcd->dsim, DCS_WR_NO_PARA, wbuf[0], 0);
+		ret = lcd->dsim->ops->cmd_write(lcd->dsim, DCS_WR_NO_PARA, wbuf[0], 0);
 	else if (size == 2)
-		lcd->dsim->ops->cmd_write(lcd->dsim, DCS_WR_1_PARA, wbuf[0], wbuf[1]);
+		ret = lcd->dsim->ops->cmd_write(lcd->dsim, DCS_WR_1_PARA, wbuf[0], wbuf[1]);
 	else
-		lcd->dsim->ops->cmd_write(lcd->dsim, DCS_LONG_WR, (unsigned int)wbuf, size);
+		ret = lcd->dsim->ops->cmd_write(lcd->dsim, DCS_LONG_WR, (unsigned int)wbuf, size);
 
 	mutex_unlock(&lcd->lock);
 
-	return 0;
+	return ret;
+}
+
+static int s6d6aa1_write(struct lcd_info *lcd, const unsigned char *seq, int len)
+{
+	int ret = 0;
+	int retry_cnt = 1;
+
+retry:
+	ret = _s6d6aa1_write(lcd, seq, len);
+	if (!ret) {
+		if (retry_cnt) {
+			dev_dbg(&lcd->ld->dev, "%s :: retry: %d\n", __func__, retry_cnt);
+			retry_cnt--;
+			goto retry;
+		} else
+			dev_dbg(&lcd->ld->dev, "%s :: 0x%02x\n", __func__, seq[1]);
+	}
+
+	return ret;
 }
 
 static int _s6d6aa1_read(struct lcd_info *lcd, const u8 addr, u16 count, u8 *buf)
@@ -356,7 +376,7 @@ static int s6d6aa1_ldi_init(struct lcd_info *lcd)
 {
 	int ret = 0;
 
-	usleep_range(15000, 15000);
+	usleep_range(20000, 20000);
 
 	s6d6aa1_write(lcd, SEQ_SLPOUT, ARRAY_SIZE(SEQ_SLPOUT));
 
